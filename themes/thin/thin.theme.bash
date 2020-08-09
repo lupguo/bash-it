7084d9b427c0090e61a6eb202f3c876adfe38f57
lupingguo <lupingguo@tencent.com>
2020-08-09 15:39:55 +0800

    update error code prompt

--- themes/thin/thin.theme.bash
+++ themes/thin/thin.theme.bash
@@ -2,14 +2,15 @@
 
 function prompt_command() {
   PS1="\n[\[\e[32m\]\u\[\e[0m\]@\[\e[35m\]\h \[\e[36;1m\]\w\[\e[0m\]] \
-\e[33m{$(date +'%H:%M:%S')}\
-${reset_color}$(scm_prompt_info) \
-$(RETVAL="$?"; 
-  if [ "$RETVAL" -eq 0 ];then
+$(RETVAL="$?";
+  echo -en "\e[33m{$(date +'%H:%M:%S')}"
+  echo -en "\e[0m$(scm_prompt_info) "
+  if [ "$RETVAL" == "0" ];then
     echo -en "\[\e[32m\]($RETVAL)"
   else
     echo -en "\[\e[31;1m\a\]($RETVAL)"
-  fi)\n\
+  fi
+)
 \[\e[0;1;34m\]\$ \[\e[0m\]"
 }
 
