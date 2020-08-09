a486d40f32cad9eedbb632a306caed8d9090f4a7
lupingguo <lupingguo@tencent.com>
2020-08-09 14:57:11 +0800

    update code indented with 2 spaces

--- themes/thin/thin.theme.bash
+++ themes/thin/thin.theme.bash
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 function prompt_command() {
-    PS1="\n[\[\e[32m\]\u\[\e[0m\]@\[\e[35m\]\h \[\e[36;1m\]\w\[\e[0m\]] \
+  PS1="\n[\[\e[32m\]\u\[\e[0m\]@\[\e[35m\]\h \[\e[36;1m\]\w\[\e[0m\]] \
 \e[33m{$(date +'%H:%M:%S')}\
 ${reset_color}$(scm_prompt_info) \
 $(RETVAL="$?"; 
-    if [ "$RETVAL" -eq 0 ];then
-        echo -en "\[\e[32m\]($RETVAL)"
-    else
-        echo -en "\[\e[31;1m\a\]($RETVAL)"
-    fi)\n\
+  if [ "$RETVAL" -eq 0 ];then
+    echo -en "\[\e[32m\]($RETVAL)"
+  else
+    echo -en "\[\e[31;1m\a\]($RETVAL)"
+  fi)\n\
 \[\e[0;1;34m\]\$ \[\e[0m\]"
 }
 
